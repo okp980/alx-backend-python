3ffec177dc00969c131fa84eb0d701f82bc34bbc
Okpunor Emmanuel <okpunorrex@gmail.com>
2025-10-05 18:16:07 +0300

    Remove chat URL routing from the messaging app's URL configuration to streamline API endpoints.

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('chats/', include('chats.urls')),
 ]
