1191da7652e3012615410f0be4939173479a09da
Okpunor Emmanuel <okpunorrex@gmail.com>
2025-10-05 18:47:16 +0300

    Update pagination settings in the messaging app to use PageNumberPagination with a page size of 20, enhancing user experience for message retrieval.

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -140,7 +140,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
         
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
 }
 
